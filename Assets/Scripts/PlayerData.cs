7f2daa4c07c81a66bc13d733965fa3d0722aca7d
LEMON48\chinc <azumasato24@gmail.com>
2025-09-25 23:25:58 +0900

    作り直し2

--- Assets/Scripts/PlayerData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "PlayerData")]
-public class PlayerData : ScriptableObject
-{
-    public string playerName;   //プレイヤー名
-    public Color color;         //プレイヤーカラー
-    public int money;           //所持資金
-}
